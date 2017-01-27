ed5ad573795213ef34e9b156270211ddcd8a1b65
Solomin Andrey <solomin.andrey@gmail.com>
2017-01-27 21:28:56 +0300

    added artists router and artists array

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var app = express();
+var artists = [
+    {
+        id: 1,
+        name: 'Metallica'
+    },
+    {
+        id: 2,
+        name: 'Aria'
+    },
+    {
+        id: 3,
+        name: 'Iron Maiden'
+    }
+];
 
 app.get('/', function (req, res) {
     res.send('Hello from NodeJS Server');
+});
+
+app.get('/artists', function (req, res) {
+    res.send(artists);
 });
 
 app.listen(3012, function () {
