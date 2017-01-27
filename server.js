3d5154009370cefb09048f30031920ceead1b5a8
Solomin Andrey <solomin.andrey@gmail.com>
2017-01-27 21:22:24 +0300

    added home router and request listener

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function (req, res) {
+    res.send('Hello from NodeJS Server');
+});
 
+app.listen(3012, function () {
+    console.log('API Server started')
+});
 
