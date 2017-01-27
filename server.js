4c4f8a672b527dcbd6ab9818f1a11bb4a1d3028c
Solomin Andrey <solomin.andrey@gmail.com>
2017-01-27 22:32:05 +0300

    added delete from id

--- server.js
+++ server.js
@@ -43,6 +43,21 @@ app.post('/artists', function (req, res) {
     res.send(artist);
 });
 
+app.put('/artists/:id', function (req, res) {
+    var artist = artists.find(function (artist) {
+        return artist.id === Number(req.params.id);
+    });
+    artist.name = req.body.name;
+    res.sendStatus(200);
+});
+
+app.delete('/artists/:id', function (req, res) {
+    artists = artists.filter(function (artist) {
+        return artist.id !== Number(req.params.id);
+    });
+    res.sendStatus(200);
+});
+
 app.listen(3012, function () {
     console.log('API Server started')
 });
